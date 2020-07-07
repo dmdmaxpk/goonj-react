184e434c9db9450c474656193892494fe1641674
Zain Mukhtar <zain.mukhtar@dmdmax.com>
2020-07-07 22:09:22 +0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ class App extends React.Component {
       <div>
         {this.props.location.pathname !== "/binjee" ?
           <div>
-            {/* <Header currentRoute={this.props.location.pathname} /> 
-             <Sidebar position={"left"} />  */}
+            <Header currentRoute={this.props.location.pathname} /> 
+             <Sidebar position={"left"} /> 
           </div>
           :
           ''
