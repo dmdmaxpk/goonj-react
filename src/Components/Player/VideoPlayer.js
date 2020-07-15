040bbe20dba3ca770dbcadad9e5faa9fc0e22a66
Taha <taha@dmdmax.com.pk>
2020-07-15 20:49:44 +0500

    videojs

--- src/Components/Player/VideoPlayer.js
+++ src/Components/Player/VideoPlayer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Plyr from 'plyr';
+// import Plyr from 'plyr';
 import config from '../../Utils/config';
 import Hls from 'hls.js';
 import './LiveVP.scss';
@@ -8,6 +8,8 @@ import AxiosInstance from '../../Utils/AxiosInstance';
 import Loader from '../Loader/Loader';
 import SocialShare from '../SocialShare/SocialShare';
 import Akamai from 'akamai-auth-token';
+import videojs from 'video.js';
+import "video.js/dist/video-js.css";
 
 class VideoPlayer extends Component {
     constructor(props){
@@ -45,45 +47,57 @@ class VideoPlayer extends Component {
             let data = res.data[0];
             this.setState({data, thumbnail: data.thumbnail}, function(){
                 console.log(this.state.data);
-                // akamai_auth.setConfig({
-                //     algo: "SHA256",
-                //     key: "4db8dd0a0cf9271e4f7fe2fe8ded6fe3",
-                //     window: 500,
-                //     acl: '/*', //optional
-                //     start_time: 0,
-                //     // session_id:{id}, //optional
-                //     // url: {url}, //optional
-                // });
-                // let generatedToken = akamai_auth.generateToken();
-                // let token = generatedToken;
+                akamai_auth.setConfig({
+                    algo: "SHA256",
+                    key: "72fb58000a0d1561f60da877b5a009fb",
+                    window: 500,
+                    acl: '/*', //optional
+                    start_time: 0,
+                    // session_id:{id}, //optional
+                    // url: {url}, //optional
+                });
+                let generatedToken = akamai_auth.generateToken();
+                let token = generatedToken;
                 const source = `${this.state.urlLink}/${this.state.data.hls_link.split('.')[0]}_360p/index.m3u8?hdnts=${token}`;
                 console.log("url", source);
                 this.setState({source});
-                const video = document.querySelector('video');
+
+                this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+                    console.log('onPlayerReady', this)
+                });
+
+                this.player.src({
+                    src: source,
+                    type: "application/x-mpegURL",
+                    withCredentials: true
+                  });
+                
+
+                // const video = document.querySelector('video');
                 
                 // For more options see: https://github.com/sampotts/plyr/#options
                 // captions.update is required for captions to work with hls.js
-                const player = new Plyr(video, {captions: {active: true, update: true, language: 'en'}});
+                // const player = new Plyr(video, {captions: {active: true, update: true, language: 'en'}});
                 
-                if (!Hls.isSupported()) {
-                    video.src({
-                        src: source,
-                        type: "application/x-mpegURL",
-                        withCredentials: true
-                      });
-                } else {
-                    // For more Hls.js options, see https://github.com/dailymotion/hls.js
-                    const hls = new Hls();
-                    hls.loadSource(source);
-                    hls.attachMedia(video);
-                    window.hls = hls;
+                // if (!Hls.isSupported()) {
+                //     video.src({
+                //         src: source,
+                //         type: "application/x-mpegURL",
+                //         withCredentials: true
+                //       });
+                // } else {
+                //     // For more Hls.js options, see https://github.com/dailymotion/hls.js
+                //     const hls = new Hls();
+                //     hls.loadSource(source);
+                //     hls.attachMedia(video);
+                //     window.hls = hls;
                     
-                    // Handle changing captions
-                    player.on('languagechange', () => {
-                        // Caption support is still flaky. See: https://github.com/sampotts/plyr/issues/994
-                        setTimeout(() => hls.subtitleTrack = player.currentTrack, 50);
-                    });
-                }
+                //     // Handle changing captions
+                //     player.on('languagechange', () => {
+                //         // Caption support is still flaky. See: https://github.com/sampotts/plyr/issues/994
+                //         setTimeout(() => hls.subtitleTrack = player.currentTrack, 50);
+                //     });
+                // }
                 // Expose player so it can be used from the console
                 // window.player = player;
         })
@@ -100,9 +114,9 @@ class VideoPlayer extends Component {
         let {data} = this.state;
         return(
             this.state.data.length !== 0 ? 
-                <div className="videoPlayerContainer" style={{width: "1200px", height: "500px", padding: "0 10%"}}>
-                    <video className="liveVP" autoPlay controls crossOrigin playsInline poster={`${config.channelLogoUrl}/${this.state.thumbnail.split(".")[0]}.jpg`}>
-                        <source src={this.state.source} type="application/x-mpegURL"  />
+                <div className="videoPlayerContainer" style={{width: "1200px", height: "500px", padding: "0 10%"}} data-vjs-player>
+                    <video ref={ node => this.videoNode = node } id='channel-player' className="liveVP" controls poster={`${config.channelLogoUrl}/${this.state.thumbnail.split(".")[0]}.jpg`} data-setup='{"fluid": true, "autoplay": false, "preload": none}' webkit-playsinline>
+                        {/* <source src={this.state.source} type="application/x-mpegURL"  /> */}
                         {/* <!-- Caption files --> */}
                         <track kind="captions" label="Urdu" srcLang="ur" src="" default />
                         {/* <!-- Fallback for browsers that don't support the <video> element --> */}
