d2ccaf0f9153bd094b0310785a9848b155837dbe
Taha <taha@dmdmax.com.pk>
2020-07-16 12:46:38 +0500

    pagination reload bug

--- src/Pages/VOD/ChannelVods.js
+++ src/Pages/VOD/ChannelVods.js
@@ -19,27 +19,33 @@ class ChannelVodPage extends Component {
             data: [],
             skip: 60,
             limit: 60,
-            page: this.props.match.params.pageNumber
+            page: this.props.match.params.pageNumber,
+            loading: false
         }
         this.handleClick = this.handleClick.bind(this);
         this.getVodUrl = this.getVodUrl.bind(this);
+        this.getVideos = this.getVideos.bind(this);
     }
     componentDidMount(){
         window.scrollTo({
             top: 0,
             behavior: "smooth"
         });
-
+        this.getVideos();
+    }
+    getVideos(){
+        this.setState({loading: true});
         let apiUrl = `/video?source=${this.props.match.params.source}&limit=${this.state.limit}&skip=${this.state.skip * (this.props.match.params.pageNumber - 1)}`;
-        
         AxiosInstance.get(apiUrl)
         .then(res =>{
-            this.setState({data: res.data});
+            this.setState({data: res.data, loading: false});
         })
     }
     componentWillReceiveProps(nextProps) {
         if(this.props.match.params.pageNumber !== nextProps.match.params.pageNumber) {
-            window.location.reload();
+            this.setState({page: this.props.match.params.pageNumber}, function(){
+                this.componentDidMount();
+            })
         }
     }
     
@@ -64,7 +70,7 @@ class ChannelVodPage extends Component {
                     <CategoryDD category={this.props.match.params.category} />
                 </div>
                 <GridContainer>
-                    {this.state.data.length > 1 ?
+                    {this.state.loading === false ?
                         this.state.data.map(item =>
                             <GridItem className="vodGridItem" xs={6} md={6} lg={2}>
                                 <div className="imgDiv" onClick={()=> this.handleClick(item)}>
@@ -84,6 +90,7 @@ class ChannelVodPage extends Component {
                     }
                     <GridItem sm={12} md={12} xs={12}>
                         <div className="paginationDiv">
+                        {this.state.data.length >= this.state.limit ?
                             <Pagination
                                 page={parseInt(this.props.match.params.pageNumber)}
                                 className="pagination"
@@ -97,6 +104,9 @@ class ChannelVodPage extends Component {
                                     />
                                 )}
                             />
+                            :
+                            ''
+                        }
                         </div>
                     </GridItem>
                 </GridContainer>
