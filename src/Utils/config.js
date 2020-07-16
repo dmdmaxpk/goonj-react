904f18a9f743e8627ee2ad98a949ac13463bebd0
Taha <taha@dmdmax.com.pk>
2020-07-16 14:56:14 +0500

    alpha config

--- src/Utils/config.js
+++ src/Utils/config.js
@@ -6,8 +6,10 @@ const config = {
         channelLogoUrl: "https://content-dmd.s3.eu-central-1.amazonaws.com/TP-Content/static-content/live/logo",
         videoLogoUrl: "https://content-dmd.s3.eu-central-1.amazonaws.com/TP-Content/static-content/video/thumb",
         hepage: "http://localhost/hepage",
-        liveStreamUrl: "//weblive.goonj.pk",
-        streamKey: '4db8dd0a0cf9271e4f7fe2fe8ded6fe3'
+        liveStreamUrl: "//kaios.streamax.io",
+        streamKey: '72fb58000a0d1561f60da877b5a009fb'
+        // liveStreamUrl: "//weblive.goonj.pk",
+        // streamKey: '4db8dd0a0cf9271e4f7fe2fe8ded6fe3'
     },
     staging: {
         apiBaseUrl: 'https://api.goonj.pk/v2',
@@ -15,10 +17,10 @@ const config = {
         channelLogoUrl: "https://content-dmd.s3.eu-central-1.amazonaws.com/TP-Content/static-content/live/web-thumb",
         videoLogoUrl: "https://content-dmd.s3.eu-central-1.amazonaws.com/TP-Content/static-content/video/thumb",
         hepage: "http://hepage.goonj.pk",
-        // liveStreamUrl: "//kaios.streamax.io",
-        // streamKey: '72fb58000a0d1561f60da877b5a009fb'
-        liveStreamUrl: "//weblive.goonj.pk",
-        streamKey: '4db8dd0a0cf9271e4f7fe2fe8ded6fe3'
+        liveStreamUrl: "//kaios.streamax.io",
+        streamKey: '72fb58000a0d1561f60da877b5a009fb'
+        // liveStreamUrl: "//weblive.goonj.pk",
+        // streamKey: '4db8dd0a0cf9271e4f7fe2fe8ded6fe3'
     },
     production: {
         apiBaseUrl: "https://api.goonj.pk/v2",
@@ -26,8 +28,10 @@ const config = {
         channelLogoUrl: "https://content-dmd.s3.eu-central-1.amazonaws.com/TP-Content/static-content/live/logo",
         videoLogoUrl: "https://content-dmd.s3.eu-central-1.amazonaws.com/TP-Content/static-content/video/thumb",
         hepage: "http://hepage.goonj.pk",
-        liveStreamUrl: "//weblive.goonj.pk",
-        streamKey: '4db8dd0a0cf9271e4f7fe2fe8ded6fe3'
+        liveStreamUrl: "//kaios.streamax.io",
+        streamKey: '72fb58000a0d1561f60da877b5a009fb'
+        // liveStreamUrl: "//weblive.goonj.pk",
+        // streamKey: '4db8dd0a0cf9271e4f7fe2fe8ded6fe3'
     }
 }
 
