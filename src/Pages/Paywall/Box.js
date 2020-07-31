ae3b3bc876cf6b224e7f180a2f73e1e4246b80f0
Taha <taha@dmdmax.com.pk>
2020-07-31 16:19:42 +0500

    box design changes

--- src/Pages/Paywall/Box.js
+++ src/Pages/Paywall/Box.js
@@ -2,10 +2,9 @@ import React from 'react';
 import PaywallInstance from '../../Utils/PaywallInstance';
 import  config  from '../../Utils/config';
 import { withRouter } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
+import ArrowForwardRoundedIcon from '@material-ui/icons/ArrowForwardRounded';
 import "./paywall.scss";
-// import { Event } from '../Tracking';
-// import Loader from 'react-loader-spinner';
 
 class Box extends React.Component {
 
@@ -25,29 +24,32 @@ class Box extends React.Component {
             loading: false
         }
         this.handleChange = this.handleChange.bind(this);
+        this.selectPayment = this.selectPayment.bind(this);
         this.sendOtp = this.sendOtp.bind(this);
         this.verifyOtp = this.verifyOtp.bind(this);
         this.subscribe = this.subscribe.bind(this);
         this.cancel = this.cancel.bind(this);
     }
     componentDidMount(){
-        // console.log(this.props);
     }
     handleChange(e){
-        // console.log(e.target.name, e.target.value);
         if(e.target.value.length < 12){
             this.setState({
                 [e.target.name]: e.target.value
             })
         }
     }
-
-    sendOtp(e){
-        //e.preventDefault();
-        console.log('sendOtp: ', e);
-        const {msisdn} = this.state;
+    selectPayment(paymentType){
+        console.log("payment", paymentType)
+        this.setState({
+            paymentType,
+            step: 1
+        })
+    }
+    sendOtp(){
+        const {msisdn, paymentType} = this.state;
         const {source, packageID2} = this.props;
-        const payment_source = e;
+        const payment_source = paymentType;
         const msisdnData = {
             msisdn,
             source,
@@ -62,24 +64,22 @@ class Box extends React.Component {
                 .then(res =>{
                     const result = res.data;
                     if(result.code === 0){
-                        this.setState({step: 1, paymentType: payment_source});
+                        this.setState({step: 2});
                     }
                     else if(result.code === -1){
                         console.log('result.message: ', result.message);
                         console.log('result: ', result);
                         alert(result.message);
-                        // console.log(result);
                     }
                 })
                 .catch(err =>{
-                    // console.log(err)
                     console.log('err: ', err);
                     alert(err)
                 })
         }
         else{
             let paymentSource = payment_source.charAt(0).toUpperCase() + payment_source.slice(1);
-            let message = 'Please Enter a Valid '+(payment_source == 'telenor' ? paymentSource+' Number' : paymentSource+' Acount Number');
+            let message = 'Please Enter a Valid '+(payment_source == 'telenor' ? paymentSource+' Number' : paymentSource+' Account Number');
             alert(message);
         }
     }
@@ -108,7 +108,7 @@ class Box extends React.Component {
                 }
                 else if(result.subscription_status === "graced" && result.is_allowed_to_stream === false){
                     this.setState({
-                        step: 2
+                        step: 3
                     })
                 }
                 else if(result.code == 7){
@@ -166,11 +166,6 @@ class Box extends React.Component {
                     localStorage.setItem(msisdnKey, msisdn);
                     this.props.history.push(`${url}`);
                 }
-                // if(res.data.code === 11 && res.data.message === "Trial period activated!"){
-
-                // }
-                // else if(res.data.code === 10 && res.data.message === "In queue for billing!"){
-                // }
             })
             .catch(err =>{
                 this.setState({loading: false});
@@ -187,38 +182,50 @@ class Box extends React.Component {
                 {
                     this.state.step === 0 ?
                         <div>
-                            <input className="msisdnInput" type="number" name="msisdn" maxLength="11" value={this.state.msisdn} placeholder="03xxxxxxxxx" pattern="\d*" onChange={this.handleChange}/>
-                            <br />
                             <p>Subscribe Now</p>
-                            <button className="btnSub" onClick={() => this.sendOtp('telenor')}>
+                            <button className="btnSub" onClick={()=> this.selectPayment('telenor')}>
                                 <img className="btnSubImg" src={require("../../Assets/telenorBtn.png")} />
                             </button>
-                            <button className="btnSub" onClick={() => this.sendOtp('easypaisa')}>
+                            <p className="orText">- OR -</p>
+                            <button className="btnSub" onClick={()=> this.selectPayment('easypaisa')}>
                                 <img className="btnSubImg" src={require("../../Assets/easypaisaBtn.png")} />
                             </button>
                         </div>
+                    :
+                    this.state.step === 1 ?
+                        <div>
+                            <p>Enter {this.state.paymentType == "easypaisa" ? `${this.state.paymentType} account` : this.state.paymentType} number</p>
+                            <input className="msisdnInput" type="number" name="msisdn" maxLength="11" value={this.state.msisdn} placeholder="03xxxxxxxxx" pattern="\d*" onChange={this.handleChange}/>
+                            <br />
+                            <button className="arrowBtnBack" onClick={()=> this.setState({step: 0})}>
+                                <ArrowBackRoundedIcon />
+                            </button>
+                            <button className="arrowBtnForward" onClick={this.sendOtp}>
+                                <ArrowForwardRoundedIcon />
+                            </button>
+                        </div>
+                    :
+                    this.state.step === 2 ?
+                        <div>
+                            <label className="otpLabel"> Enter your OTP received through sms </label>
+                            <p className="otpText">Enter OTP*</p>
+                            <input className="msisdnInput" type="number" name="otp" value={this.state.otp} placeholder="xxxx" pattern="\d*" onChange={this.handleChange}/>
+                            <br />
+                            <button className="arrowBtnBack" onClick={()=> this.setState({step: 1})}>
+                                <ArrowBackRoundedIcon />
+                            </button>
+                            <button className="arrowBtnForward" onClick={this.state.paymentType === 'telenor' ? this.verifyOtp : this.subscribe}>
+                                <ArrowForwardRoundedIcon />
+                            </button>
+                        </div>
                         :
-                        this.state.step === 1 ?
-                            <div>
-                                <label className="otpLabel"> Enter OTP </label>
-                                <br />
-                                <input className="msisdnInput" type="number" name="otp" value={this.state.otp} placeholder="xxxxx" pattern="\d*" onChange={this.handleChange}/>
-                                <br />
-                                <button className="btnSub" onClick={this.state.paymentType === 'telenor' ? this.verifyOtp : this.subscribe}>
-                                    <div className="button_outer_div">
-                                        <img className="btnNext" src={require("../../Assets/Shape-2.png")} />
-                                        <p className="btnNextText">NEXT</p>
-                                    </div>
-                                </button>
-                            </div>
-                            :
-                            <div className="">
-                                <p className="text1">Are you sure<br />you want to subscribe?</p>
-                                <button className="btnSubConfirm" onClick={this.subscribe}>
-                                    <img className="confirmBtnImg" src={require("../../Assets/Shape-2.png")} />
-                                    <p className="btnConfirmText">Confirm</p>
-                                </button>
-                            </div>
+                        <div className="">
+                            <p className="text1">Are you sure<br />you want to subscribe?</p>
+                            <button className="btnSubConfirm" onClick={this.subscribe}>
+                                <img className="confirmBtnImg" src={require("../../Assets/Shape-2.png")} />
+                                <p className="btnConfirmText">Confirm</p>
+                            </button>
+                        </div>
                 }
             </div>
         );
