d0c239ca7aa6f1342de7ac6e8469863134b731ce
Zain Mukhtar <67791976+zaindmdmax@users.noreply.github.com>
2020-08-04 15:56:03 +0500

    minorchanges

--- src/Pages/StaticPages/UnSubPage.js
+++ src/Pages/StaticPages/UnSubPage.js
@@ -11,7 +11,6 @@ export default class UnSubPage extends Component {
       user_ID: new URLSearchParams(this.props.location.search).get("proxy"),
       sourcee: "sms",
       package_ID: new URLSearchParams(this.props.location.search).get("pg"),
-      alreadySubscribed: false,
       loading: true,
       messageText: "",
     };
@@ -23,36 +22,37 @@ export default class UnSubPage extends Component {
       source: this.state.sourcee,
       package_id: this.state.package_ID,
     };
-
+    if(this.state.user_ID!=null){
     AxiosInstance.post("payment/status", unsubData)
       .then((res) => {
         let result = res.data;
         if (result.code == -1 && result.message) {
-          this.setState({ messageText: "UserID is incorrect" });
-        }else if (result.data.auto_renewal==true) {
+          this.setState({ messageText: "User does not exist." });
+        } else if (result.data.auto_renewal == true) {
           AxiosInstance.post("payment/unsubscribe", unsubData).then((res) => {
             this.setState({ messageText: "You have been Unsubscribed" });
           });
-        }else if (result.data.auto_renewal==false){
-            this.setState({ messageText: "You have already been unsubscribed" });
-        }
-         else if (result.code == -1 && !result.data.auto_renewal) {
+        } else if (result.data.auto_renewal == false) {
+          this.setState({ messageText: "You are already Unsubscribed'" });
+        } else if (result.code == -1 && !result.data.auto_renewal) {
           this.setState({ messageText: "You do not have this package" });
         }
         this.setState({ loading: false });
       })
       .catch((err) => {
-        // console.log(err);
+        console.log(err);
       });
+    }else{
+      this.setState({ messageText: "Sorry! Nothing to display" });
+      this.setState({ loading: false });
+    }
   }
+
 
   render() {
     return (
       <div>
-        {console.log(
-          this.state.user_idd + "   Hello  " + this.state.package_idd
-        )}
-        <GridContainer className="containerPP">
+        <GridContainer className="containerUnSub">
           <GridItem xs={12} sm={12} md={12}>
             {this.state.loading === false ? (
               <p className="d-flex justify-content-center">
