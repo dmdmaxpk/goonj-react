eb9d71cb85b6367f2055dbac63d70f89466ae5d1
Zain Mukhtar <67791976+zaindmdmax@users.noreply.github.com>
2020-08-10 13:15:14 +0500

    component load on scroll

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,7 @@ import ChannelList from '../../Components/ListSections/ChannelList';
 import DramasSection from '../../Components/HomeSections/Dramas';
 import VodSection from '../../Components/HomeSections/Vod';
 import PopularList from '../../Components/ListSections/PopularList';
+import InfiniteScroll from "react-infinite-scroll-component";
 import Loader from '../../Components/Loader/Loader';
 import './Home.scss';
 import HeadlinesSection from '../../Components/HomeSections/Headlines';
@@ -12,14 +13,46 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state ={
-            loading: true
+            loading: true,
+            items: Array.from({ length: 3 }),
+            hasMore: true
         }
     }
+
+    fetchMoreData = () => {
+        if (this.state.items.length >= 6) {
+          this.setState({ hasMore: false });
+          return;
+        }
+        setTimeout(() => {
+          this.setState({
+            items: this.state.items.concat(Array.from({ length: 1 }))
+          });
+        }, 500);
+      };
+
   
     componentWillMount(){
         this.setState({
             loading: false
         })
+    }
+
+    renderComponent(e){
+        console.log(e+' This is the value')
+        if(e==0){
+            return  <PopularList title="Popular on Goonj" class="popularContainer" />
+        }else if(e==1){
+            return     <ChannelList classname="channelList"/>
+        }else if(e==2){
+            return <DramasSection category="entertainment" /> 
+        }else if(e==3){
+            return <HeadlinesSection style={{top:"2%"}} category="news" title="Headlines" />
+        }else if(e==4){
+            return <VodSection apiLink={`/video?category=sports&limit=5`} title="Sports" category="sports"  classname="sportsContainer" />
+        }else if(e==5){
+            return <VodSection title="Programs" apiLink={`/video?category=programs&limit=5`} category="programs" classname="programsContainer" />
+        }
     }
     render(){
         return(
@@ -32,16 +65,32 @@ class Home extends Component {
                     <div className="homeContainer">
                         <PosterSlider />
                         <div className="homeSections">
-                            <PopularList title="Popular on Goonj" class="popularContainer" />
-                         
+                        <InfiniteScroll
+                                        dataLength={this.state.items.length}
+                                        next={this.fetchMoreData}
+                                        hasMore={this.state.hasMore}
+                                        loader={<h4>Loading...</h4>}
+                                        endMessage={
+                                        <p style={{ textAlign: "center" }}>
+                                            <b>Yay! You have seen it all</b>
+                                        </p>
+                                        }
+                                        >
+                                        {this.state.items.map((i, index) => (
+                                            <div>{this.renderComponent(index)}
+                                             <br/></div> 
+                                        ))}
+                                        
+                                   
+                                    </InfiniteScroll>
+
+
+                            {/* <PopularList title="Popular on Goonj" class="popularContainer" />
                             <ChannelList classname="channelList"/>
-                           
                             <DramasSection category="entertainment" /> 
                             <HeadlinesSection style={{top:"2%"}} category="news" title="Headlines" />
-                         
                             <VodSection apiLink={`/video?category=sports&limit=5`} title="Sports" category="sports"  classname="sportsContainer" />
-                        
-                            <VodSection title="Programs" apiLink={`/video?category=programs&limit=5`} category="programs" classname="programsContainer" />
+                            <VodSection title="Programs" apiLink={`/video?category=programs&limit=5`} category="programs" classname="programsContainer" /> */}
                         </div>
                     </div>
                 }
