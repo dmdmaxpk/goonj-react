f9e8ec26a9cf5e3c5f7ba84418d259c6c05d4815
Taha <taha@dmdmax.com.pk>
2020-08-28 12:32:28 +0500

    worker cache deleted

--- public/worker.js
+++ public/worker.js
@@ -1,9 +1,9 @@
 var CACHE_NAME = 'Goonj';
-var urlsToCache = [
-  '/',
-  '/home',
-  '/live-tv'
-];
+// var urlsToCache = [
+//   '/',
+//   '/home',
+//   '/live-tv'
+// ];
 
 // Install a service worker
 self.addEventListener('install', event => {
@@ -11,28 +11,28 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 
   // Turn off cache
-  // event.waitUntil(
-  //   caches.keys().then(function(cacheNames) {
-  //     return Promise.all(
-  //       cacheNames.filter(function(cacheName) {
-  //         // Return true if you want to remove this cache,
-  //         // but remember that caches are shared across
-  //         // the whole origin
-  //         return true
-  //       }).map(function(cacheName) {
-  //         return caches.delete(cacheName);
-  //       })
-  //     );
-  //   })
-  // );
-  // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        // console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+          return true
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
   );
+  // Perform install steps
+  // event.waitUntil(
+  //   caches.open(CACHE_NAME)
+  //     .then(function(cache) {
+  //       // console.log('Opened cache');
+  //       return cache.addAll(urlsToCache);
+  //     })
+  // );
 });
 
 // Cache and return requests
