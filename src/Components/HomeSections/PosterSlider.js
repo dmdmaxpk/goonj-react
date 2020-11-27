3b42df156ae474e3ad779b0a4e67111687a17c9d
Taha <taha@dmdmax.com.pk>
2020-11-27 16:49:45 +0500

    posters update

--- src/Components/HomeSections/PosterSlider.js
+++ src/Components/HomeSections/PosterSlider.js
@@ -13,9 +13,14 @@ class PosterSlider extends Component {
     render(){
         let banners = [
             {
-                name: "02",
+                name: "01",
                 url: "#",
                 class: "carousel-item active"
+            },
+            {
+                name: "02",
+                url: "#",
+                class: "carousel-item"
             },
             {
                 name: "03",
@@ -27,16 +32,11 @@ class PosterSlider extends Component {
                 url: "#",
                 class: "carousel-item"
             },
-            {
-                name: "05",
-                url: "#",
-                class: "carousel-item"
-            },
-            {
-                name: "06",
-                url: "#",
-                class: "carousel-item"
-            },
+            // {
+            //     name: "06",
+            //     url: "#",
+            //     class: "carousel-item"
+            // },
         ]
         return(
             <div className="posterSlider">
@@ -46,7 +46,7 @@ class PosterSlider extends Component {
                         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                        {/* <li data-target="#carouselExampleIndicators" data-slide-to="4"></li> */}
                     </ol>
                     <div className="carousel-inner carouselDiv">
                         {
