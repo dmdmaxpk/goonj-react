ab9923206d58bb2e54434815f8f61a1dcbb21f99
Hatim Hammad <hatim@dmdmax.com>
2023-10-05 21:46:54 +0500

    Center Aligned Logo (commit#13)

--- src/App.js
+++ src/App.js
@@ -173,7 +173,7 @@ class App extends React.Component {
                   {/*!( window.location.href === 'https://goonj.pk/?source=mta' || window.location.href === 'https://goonj.pk/?source=mta2')*/}
                   
                   {/* Conditionally render the Go Back button */}
-                  { !( window.location.href === 'https://goonj.pk/?source=mta' || window.location.href === 'https://goonj.pk/?source=mta2') && 
+                  { !( window.location.href === 'http://localhost:3000/?source=mta' || window.location.href === 'http://localhost:3000/?source=mta2') && 
                   (
                     <div
                       onClick={this.goBackClickHandler}
