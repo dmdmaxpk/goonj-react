39750c0ec30628f7f4fd7b7c3aab7e6061f53304
Farhan Ali <farhan.ali@dmdmax.com>
2023-10-20 12:16:41 +0500

    Fix

--- src/Pages/VOD/VodPage.js
+++ src/Pages/VOD/VodPage.js
@@ -57,7 +57,7 @@ class VodPage extends Component {
         window.onpopstate = ()=> {
             if(this._isMounted) {
                 if(this.source === 'mta' || this.source === 'mta2') {
-                    this.props.history.push(`/?source=?${this.source}`)
+                    this.props.history.push(`/?source=${this.source}`)
                     // window.location.href = `/`
                 }
             }
