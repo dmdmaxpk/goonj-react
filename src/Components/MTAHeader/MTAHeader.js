41e9b9188c0b773b68961d329462cdd88abebb26
Hatim Hammad <hatim@dmdmax.com>
2023-10-23 21:36:02 +0500

    Logo and Routing of view more done

--- src/Components/MTAHeader/MTAHeader.js
+++ src/Components/MTAHeader/MTAHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Header.css'; // Import your CSS file
+import './Header.css';
 import Logo from "../../Assets/logo.png";
 
 function goBack() {
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,21 @@
 //Home.js
 import { withRouter } from 'react-router-dom';
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import PosterSlider from '../../Components/HomeSections/PosterSlider';
 import ChannelList from '../../Components/ListSections/ChannelList';
 import NewsChannelList from '../../Components/ListSections/NewsChannelList';
 import EntertainmentChannelList from '../../Components/ListSections/EntertainmentChannelList';
 import IslamicChannelList from '../../Components/ListSections/IslamicChannelList';
+import LiveTv from '../Live/LiveTvList';
 import DramasSection from '../../Components/HomeSections/Dramas';
 import VodSection from '../../Components/HomeSections/Vod';
 import PopularList from '../../Components/ListSections/PopularList';
 import InfiniteScroll from "react-infinite-scroll-component";
 import Loader from '../../Components/Loader/Loader';
+import './Home.scss';
 import HeadlinesSection from '../../Components/HomeSections/Headlines';
-import './Home.scss'
+import MainCategory from '../VOD/MainCategory';
+import { Close } from '@material-ui/icons';
 
 
 class Home extends Component {
@@ -26,6 +29,10 @@ class Home extends Component {
             Mta: false,
             Mta2: false
         }
+    }
+
+    componentDidMount() {
+        this.checkMta();
     }
 
     checkMta() {
@@ -74,6 +81,7 @@ class Home extends Component {
             loading: false
         })
     }
+
     
     renderComponent(e){
         const isMtaSource = this.props.location.search.includes('source=mta' || 'source=mta2');
@@ -184,7 +192,7 @@ class Home extends Component {
                         <Loader color="secondary" />
                     </div>
                 :
-                    <div className="mainHomeContainer">
+                    <div className="homeContainer">
                         <PosterSlider />
                         <div className="homeSections">
                         <InfiniteScroll
