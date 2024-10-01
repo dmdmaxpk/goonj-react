d409c000f51af146571357dca0c5c10fd234f2cd
Taha <taha@dmdmax.com>
2024-10-01 10:49:48 -0700

    ads link updated

--- src/Components/Player/VideoPlayer.js
+++ src/Components/Player/VideoPlayer.js
@@ -81,10 +81,10 @@ class VideoPlayer extends Component {
                         // this.ads(); // Ensure the ads plugin is initialized
                         this.ima({
                             id: 'content_video',
-                            adTagUrl: 'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=',
+                            adTagUrl: 'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinearvpaid2js&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=',
                             debug: true,
                         });
-                        console.log('this.player.ads', this.ima)
+                        console.log('this.player.ads', this.player().ads)
                         if (this.player.ads) {
                             this.player.ads.requestAds();
                         } else {
